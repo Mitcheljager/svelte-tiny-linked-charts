eb939460416b45aab543699c8e5d0a1c7794c060
Mitchel <mitchel_jager@hotmail.com>
2021-08-16 21:36:24 +0200

    Add additional docs to demo page

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -9,7 +9,7 @@
 			const setDate = date.setDate(date.getDate() - 1)
 			const formattedDate = new Date(setDate).toISOString().substring(0, 10)
 
-      data[formattedDate] = Math.floor(Math.random() * 100)
+      data[formattedDate] = Math.floor(Math.random() * 50) + 50
     }
 
 		const reversedData = {}
@@ -24,7 +24,10 @@
 
 
 <div class="wrapper">
-	<h1>Tiny Linked Charts for <mark>Svelte</mark></h1>
+	<div class="header">
+		<h1>Tiny Linked Charts for <mark>Svelte</mark></h1>
+		<LinkedChart data={ fakeData(108) } width="540" height="5" />
+	</div>
 
 	<div class="block">
 		<div class="description">
@@ -43,8 +46,6 @@
 			You can link multiple charts together, hovering one will also highlight others.
 
 			<code>
-				&lt;LinkedLabel linked="link-1" /&gt; <br>
-				<br>
 				&lt;LinkedChart &#123; data &#125; linked="link-1" /&gt; <br>
 				&lt;LinkedChart &#123; data &#125; linked="link-1" /&gt; <br>
 				&lt;LinkedChart &#123; data &#125; linked="link-1" /&gt; <br>
@@ -53,8 +54,6 @@
 		</div>
 
 		<div>
-			<div class="label"><LinkedLabel linked="link-1" empty="Start hovering" /></div>
-
 			<div class="chart"><LinkedChart data={ fakeData(30) } linked="link-1" /></div>
 			<div class="chart"><LinkedChart data={ fakeData(10) } linked="link-1" /></div>
 			<div class="chart"><LinkedChart data={ fakeData(30) } linked="link-1" /></div>
@@ -62,12 +61,138 @@
 		</div>
 	</div>
 
-	<div class="chart">
-		<LinkedChart data={ fakeData(10) } fadeOpacity="0.75" />
+	<div class="block">
+		<div class="description">
+			You can optionally display a label, which will display the label of what you're currently hovering.
+
+			<code>
+				&lt;LinkedLabel linked="link-2" /&gt; <br>
+				<br>
+				&lt;LinkedChart &#123; data &#125; linked="link-2" /&gt; <br>
+				&lt;LinkedChart &#123; data &#125; linked="link-2" /&gt; <br>
+			</code>
+			<br>
+			The label has no styling by default.
+		</div>
+
+		<div>
+			<div class="label"><LinkedLabel linked="link-2" empty="Start hovering" /></div>
+
+			<div class="chart"><LinkedChart data={ fakeData(30) } linked="link-2" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } linked="link-2" /></div>
+		</div>
 	</div>
 
-	<div class="chart">
-		<LinkedChart data={ fakeData(5) } grow={ true } hover={ false } barMinWidth="0" />
+	<div class="block">
+		<div class="description">
+			The width of the bars is fixed by default, but can be set to grow to fill the chart.
+
+			<code>
+				&lt;LinkedChart data=&#123 ... &#125; grow=&#123 true &#125; /&gt;
+			</code>
+		</div>
+
+		<LinkedChart data={ fakeData(5) } grow={ true } />
+	</div>
+
+	<div class="block">
+		<div class="description">
+			To make the bars smaller you can set the "barMinWidth" property.
+
+			<code>
+				&lt;LinkedChart data=&#123 ... &#125; barMinWidth="2" /&gt;
+			</code>
+		</div>
+
+		<LinkedChart data={ fakeData(50) } barMinWidth="2" />
+	</div>
+
+	<div class="block">
+		<div class="description">
+			To always fill out the content, growing and shrinking, you can set both the "grow" and "barMinWidth" properties.
+
+			<code>
+				&lt;LinkedChart<br>
+				&nbsp; data=&#123 ... &#125; <br>
+				&nbsp; grow=&#123 true &#125; <br>
+				&nbsp; barMinWidth="0" /&gt;
+			</code>
+		</div>
+
+		<div>
+			<div class="chart"><LinkedChart data={ fakeData(75) } grow={ true } barMinWidth="0" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(10) } grow={ true } barMinWidth="0" /></div>
+		</div>
+	</div>
+
+	<div class="block">
+		<div class="description">
+			The charts can be resized to any size you like. It renders as an SVG, so they can easily be made responsive with some CSS.
+
+			<code>
+				&lt;LinkedChart<br>
+				&nbsp; data=&#123 ... &#125; <br>
+				&nbsp; width="250" <br>
+				&nbsp; height="100" /&gt;
+			</code>
+
+			<code>
+				svg &#123; <br>
+				&nbsp; width: 100%; <br>
+				&nbsp; height: auto; <br>
+				&#125;
+			</code>
+
+			<br>
+			or for a fixed height;
+
+			<code>
+				svg &#123; <br>
+				&nbsp; width: 100%; <br>
+				&nbsp; height: 50px; <br>
+				&#125;
+			</code>
+		</div>
+
+		<div>
+			<div class="chart chart--responsive"><LinkedChart data={ fakeData(50) } height="100" width="250" linked="linked-3" /></div>
+			<div class="chart chart--responsive"><LinkedChart data={ fakeData(50) } height="10" width="250" linked="linked-3" /></div>
+		</div>
+	</div>
+
+	<div class="block">
+		<div class="description">
+			The gap in between bars can also be adjusted.
+
+			<code>
+				&lt;LinkedChart &#123; data &#125; gap="11" /&gt;
+			</code>
+		</div>
+
+		<LinkedChart data={ fakeData(10) } gap="11" />
+	</div>
+
+	<div class="block">
+		<div class="description">
+			The bars can be colored any way you wish.
+
+			<code>
+				&lt;LinkedChart fill="#ff00ff" /&gt; <br>
+				&lt;LinkedChart fill="rgb(255, 255, 0)" /&gt; <br>
+				&lt;LinkedChart fill="hsla(290, 55%, 50%, 1)" /&gt; <br>
+			</code>
+		</div>
+
+		<div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } fill="#e6261f" linked="link-4" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } fill="#eb7532" linked="link-4" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } fill="#f7d038" linked="link-4" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } fill="#a3e048" linked="link-4" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } fill="#49da9a" linked="link-4" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } fill="#34bbe6" linked="link-4" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } fill="#4355db" linked="link-4" /></div>
+			<div class="chart"><LinkedChart data={ fakeData(30) } fill="hsla(290, 55%, 50%, 1)" linked="link-4" /></div>
+		</div>
 	</div>
 </div>
 
@@ -75,9 +200,13 @@
 
 <style>
 	:global(body) {
-		background: #222;
+		background: #111;
 		color: white;
 		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+	}
+
+	h1 {
+		margin: 0;
 	}
 
 	code {
@@ -92,8 +221,17 @@
 		color: #ff3e00;
 	}
 
+	.header {
+		margin: 6rem 0 3rem;
+	}
+
+	:global(.header svg) {
+		width: 100%;
+		height: 5px;
+	}
+
 	.wrapper {
-		max-width: 480px;
+		max-width: 540px;
 		margin: 0 auto;
 		padding: 0 1.5rem;
 	}
@@ -103,12 +241,21 @@
 		grid-template-columns: 1fr auto;
 		grid-gap: 1.5rem;
 		justify-content: space-between;
-		padding: 1.5rem 0;
+		padding: 3rem 0;
 		border-bottom: 1px solid rgba(255, 255, 255, 0.15);
 	}
 
 	.chart {
 		margin-top: .5rem;
+	}
+
+	:global(.chart--responsive svg) {
+		width: 100%;
+		height: auto;
+	}
+
+	:global(svg g) {
+		fill: blue;
 	}
 
 	.label {
