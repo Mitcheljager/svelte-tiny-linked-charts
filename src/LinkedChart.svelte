4eaaaae8cc7fde8b6d5b9dfd9583d53da7d31319
Mitchel <mitchel_jager@hotmail.com>
2021-08-17 02:28:59 +0200

    Add optional label to chart

--- src/LinkedChart.svelte
+++ src/LinkedChart.svelte
@@ -1,9 +1,10 @@
 <script>
-  import { hoveringKey } from "./stores/tinyLinkedCharts.js"
+  import { hoveringKey, hoveringValue } from "./stores/tinyLinkedCharts.js"
 
   export let data = {}
   export let labels = []
   export let values = []
+  export let linked = ""
   export let height = 40
   export let width = 150
   export let barMinWidth = 4
@@ -12,9 +13,14 @@
   export let gap = 1
   export let fill = "#ff3e00"
   export let fadeOpacity = 0.5
-  export let linked = false
   export let hover = true
   export let transition = 0
+  export let showLabel = false
+  export let labelDefault = "&nbsp;"
+  export let labelPrepend = ""
+  export let labelAppend = ""
+
+  let uid = (Math.random() + 1).toString(36).substring(7)
   
   $: dataLength = Object.keys(data).length
   $: barWidth = grow ? getBarWidth(dataLength) : parseInt(barMinWidth)
@@ -22,6 +28,7 @@
   $: alignmentOffset = dataLength ? getAlignment() : 0
   $: linkedKey = linked || (Math.random() + 1).toString(36).substring(7)
   $: if (labels.length && values.length) data = Object.fromEntries(labels.map((_, i) => [labels[i], values[i]]))
+  $: if (showLabel && $hoveringKey[linkedKey]) $hoveringValue[uid] = data[$hoveringKey[linkedKey]]
 
   function getHighestValue() {
     return Math.max(...Object.values(data))
@@ -39,6 +46,16 @@
     if (align == "left") return 0
     return (parseInt(gap) + parseInt(width)) - ((parseInt(gap) + barWidth) * dataLength)
   }
+
+  function startHover(key) {
+    if (!hover) return
+    $hoveringKey[linkedKey] = key
+  }
+
+  function endHover() {
+    if (!hover) return
+    $hoveringKey[linkedKey] = null
+  }
 </script>
 
 
@@ -48,14 +65,15 @@
   { width }
   viewBox="0 0 { width } { height }"
   preserveAspectRatio="none"
-  on:mouseleave={ () => { $hoveringKey[linkedKey] = null } }
-  on:blur={ () => { $hoveringKey[linkedKey] = null } }>
+  on:mouseleave={ endHover }
+  on:blur={ endHover }>
 
   <g transform="translate({ alignmentOffset }, 0)" { fill }>
     { #each Object.entries(data) as [key, value], i }
       <rect
-        on:mouseover={ () => { $hoveringKey[linkedKey] = key } }
-        on:focus={ () => { $hoveringKey[linkedKey] = key } }
+        on:mouseover={ () => startHover(key, value, i) }
+        on:focus={ () => startHover(key, value, i) }
+        on:touchstart={ () => startHover(key, value, i) }
         style={ transition ? `transition: all ${ transition }ms` : null }
         opacity={ hover && $hoveringKey[linkedKey] && $hoveringKey[linkedKey] != key ? fadeOpacity : 1 }
         width={ barWidth }
@@ -65,3 +83,15 @@
     { /each }
   </g>
 </svg>
+
+{ #if showLabel }
+  <div class="tiny-linked-charts-label">
+    { #if $hoveringKey[linkedKey] }
+      { labelPrepend }
+      { $hoveringValue[uid] }
+      { labelAppend }
+    { :else }
+      { @html labelDefault }
+    { /if }
+  </div>
+{ /if }
--- src/stores/tinyLinkedCharts.js
+++ src/stores/tinyLinkedCharts.js
@@ -1,3 +1,4 @@
 import { writable } from "svelte/store"
 
-export const hoveringKey = writable([])
+export const hoveringKey = writable({})
+export const hoveringValue = writable({})
